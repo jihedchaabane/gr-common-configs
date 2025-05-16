4989cc5e19aae7c47b445e8fd935b1afa6fe5d07
jihed.chaabane <jihed.chaabanee@gmail.com>
2025-05-16 14:44:50 +0200

    ..

--- src/main/java/com/chj/gr/config/resttemplate/RestTemplateConfig.java
+++ src/main/java/com/chj/gr/config/resttemplate/RestTemplateConfig.java
@@ -1,21 +1,40 @@
 package com.chj.gr.config.resttemplate;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.security.KeyStore;
+import java.security.NoSuchAlgorithmException;
 
 import javax.net.ssl.SSLContext;
 
 import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
+import com.chj.gr.properties.ServiceParamsProperties;
+
 @Configuration
+@Order(Ordered.LOWEST_PRECEDENCE)
 public class RestTemplateConfig {
+	
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	private final ServiceParamsProperties serviceParamsProperties;
+
+	public RestTemplateConfig(ServiceParamsProperties serviceParamsProperties) {
+		super();
+		this.serviceParamsProperties = serviceParamsProperties;
+	}
 		/**
 		@TODO din't work!
 		javax:
@@ -45,11 +64,20 @@ public class RestTemplateConfig {
 					    secure-port-enabled: true
 					    non-secure-port-enabled: false
 		 */
-    @Bean("restTemplateSsl")
+
+	@Bean("restTemplateSsl")
     @LoadBalanced
     public RestTemplate restTemplateSsl() throws Exception {
-        KeyStore trustStore = KeyStore.getInstance("JKS");
-        trustStore.load(getClass().getResourceAsStream("/ms1-truststore.jks"), "jihed1234".toCharArray());
+    	KeyStore trustStore = KeyStore.getInstance("JKS");
+    	
+    	try (FileInputStream fis = new FileInputStream(serviceParamsProperties.getTruststore().getPath())) {
+    	    trustStore.load(fis, serviceParamsProperties.getTruststore().getPassword().toCharArray());
+    	} catch (IOException | NoSuchAlgorithmException e) {
+    	    logger.error("Failed to load truststore from system path..!", e);
+    	}
+        
+//      trustStore.load(getClass().getResourceAsStream(
+//    		serviceParamsProperties.getTruststore().getPath()), serviceParamsProperties.getTruststore().getPassword().toCharArray());
 
         SSLContext sslContext = org.apache.http.ssl.SSLContexts.custom()
                 .loadTrustMaterial(trustStore, null)
